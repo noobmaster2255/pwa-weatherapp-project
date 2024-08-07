7561f7a0973286c63d817d18fa46e40eea1a005c
Akhil Sanil <99110858+noobmaster2255@users.noreply.github.com>
2024-08-06 20:06:05 -0400

    2nd page completed

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+const searchContainer = document.getElementById("searchContainer");
+const searchBar = document.getElementById("searchBar");
+
+searchContainer.addEventListener("click", function (event) {
+  searchContainer.classList.add("active");
+  searchBar.focus();
+});
+
+document.addEventListener("click", function (event) {
+  if (!searchContainer.contains(event.target)) {
+    if (searchBar.value.trim() === "") {
+      searchContainer.classList.remove("active");
+    }
+  }
+});
